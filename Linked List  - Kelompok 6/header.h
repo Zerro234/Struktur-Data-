81efb69359b43b587d69aaf52aff77da69fde251
Fransiskus Juniarto <145818018+Zerro234@users.noreply.github.com>
2024-05-19 16:52:23 +0700

    Add files via upload

---
+++ Linked List  - Kelompok 6/header.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+struct Mahasiswa {
+    string nama;
+    int kehadiran[14];
+    Mahasiswa* next;
+    int jumlahhadir;
+};
+
+typedef Mahasiswa* simpul;
+
+void Sisip(simpul& L, string nama);
+void TambahKehadiran(simpul& L);
+void Cetak(simpul L);
+
